017232ab2c3bcad7d1118d205ad7d7f65fac915d
zutesmog <zutesmog@gmail.com>
2009-07-21 16:01:54 +0000

    Fix missing import

--- pages/utils.py
+++ pages/utils.py
@@ -1,12 +1,19 @@
 
 import logging
 import os.path
+import os, sys
 from datetime import datetime,timedelta
 from time import mktime
 
+
+import wsgiref.util
 from wsgiref.handlers import format_date_time
 
 from google.appengine.api import memcache
+from google.appengine.api import users
+from google.appengine.ext.webapp import Request
+from google.appengine.ext.webapp.util import run_wsgi_app
+
 
 from repoze.bfg.registry import populateRegistry
 
@@ -26,6 +33,30 @@ class ActionContext(object):
         self.request = req 
         
 
+def get_cache():
+    if users.is_current_user_admin():
+        return None
+    env = dict(os.environ)
+    env["wsgi.input"] = sys.stdin
+    env["wsgi.errors"] = sys.stderr
+    env["wsgi.version"] = (1, 0)
+    env["wsgi.run_once"] = True
+    env["wsgi.url_scheme"] = wsgiref.util.guess_scheme(env)
+    env["wsgi.multithread"] = False
+    env["wsgi.multiprocess"] = False
+    req = Request(env)
+    cached_resp = memcache.get(req.path_url)
+    
+    if cached_resp:
+        def cache_app(env,start_resp):
+            logging.info('returning cached page (%s)' % req.path_url)
+            #BREAKPOINT()
+            write_handle = start_resp(cached_resp.status,(cached_resp.headers.items()))
+            write_handle(cached_resp.body)
+        return cache_app
+    else:
+
+        return None
 
 
 def cacheoutput(func):
